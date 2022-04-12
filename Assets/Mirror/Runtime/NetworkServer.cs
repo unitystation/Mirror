e46ffc8ab28b5e7f14f2ff5907073c0997ebc081
Bod9001 <Bod9001@gmail.com>
2022-04-12 23:58:00 +0100

    Is it actually Threading causing the issue?

--- Assets/Mirror/Runtime/NetworkServer.cs
+++ Assets/Mirror/Runtime/NetworkServer.cs
@@ -1684,7 +1684,12 @@ namespace Mirror
             FrameCountCash = Time.frameCount;
             ApplicationIsPlayingCash = Application.isPlaying;
 
-            Parallel.ForEach(connectionsCopy, SubConnectionBroadcast);
+            //Parallel.ForEach(connectionsCopy, SubConnectionBroadcast);
+
+            foreach (var connection in connectionsCopy)
+            {
+                SubConnectionBroadcast(connection);
+            }
 
             // TODO this is way too slow because we iterate ALL spawned :/
             // TODO this is way too complicated :/
