26b6eb8b9f5646ff71fdc89e8300c2d084dff9b1
Bod9001 <Bod9001@gmail.com>
2023-01-08 17:59:02 +0000

    Removes unneeded Optimisation

--- Assets/Mirror/Runtime/NetworkWriterPool.cs
+++ Assets/Mirror/Runtime/NetworkWriterPool.cs
@@ -13,145 +13,43 @@ namespace Mirror
     }
 
     /// <summary>Pool of NetworkWriters to avoid allocations.</summary>
+    /// <summary>Pool of NetworkWriters to avoid allocations.</summary>
     public static class NetworkWriterPool
     {
-        /// UNITYSTATION CODE /// multiple pools to reduce locks waiting
-        private static volatile bool ZeroLocked = false;
-        private static volatile bool OneLocked = false;
-        private static volatile bool TwoLocked = false;
-        private static volatile int LowestScore = 999999999;
-
-
         // reuse Pool<T>
         // we still wrap it in NetworkWriterPool.Get/Recycle so we can reset the
         // position before reusing.
         // this is also more consistent with NetworkReaderPool where we need to
         // assign the internal buffer before reusing.
-        //CUSTOM UNITYSTATION CODE// So it can be safely gotten, Without Thread funnies and size was reduced because we don't care about some GC on the initial frames
-        private static readonly List<Pool<PooledNetworkWriter>> Pools =
-            new List<Pool<PooledNetworkWriter>>()
-            {
-                new Pool<PooledNetworkWriter>(
-                    () => new PooledNetworkWriter(),
-                    // initial capacity to avoid allocations in the first few frames
-                    // 1000 * 1200 bytes = around 1 MB.
-                    100
-                ),
-                new Pool<PooledNetworkWriter>(
-                    () => new PooledNetworkWriter(),
-                    // initial capacity to avoid allocations in the first few frames
-                    // 1000 * 1200 bytes = around 1 MB.
-                    100
-                ),
-                new Pool<PooledNetworkWriter>(
-                    () => new PooledNetworkWriter(),
-                    // initial capacity to avoid allocations in the first few frames
-                    // 1000 * 1200 bytes = around 1 MB.
-                    100
-                ),
-
-            };
-
-        /// UNITYSTATION CODE /// which pool is low
-        private static List<Pool<PooledNetworkWriter>> Lowest = new List<Pool<PooledNetworkWriter>>()
-        {
-            Pools[0],
-            Pools[1],
-            Pools[2]
-        };
-        /// UNITYSTATION CODE /// which pool is low
-        private static volatile int LowestIndex = 0;
+        static readonly Pool<PooledNetworkWriter> Pool = new Pool<PooledNetworkWriter>(
+            () => new PooledNetworkWriter(),
+            // initial capacity to avoid allocations in the first few frames
+            // 1000 * 1200 bytes = around 1 MB.
+            1000
+        );
 
         /// <summary>Get a writer from the pool. Creates new one if pool is empty.</summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static PooledNetworkWriter GetWriter()
         {
-            /// UNITYSTATION CODE /// crazy logic getting the right one
             // grab from pool & reset position
-            PooledNetworkWriter writer = null;
-            if (ZeroLocked == false)
+            lock (Pool)
             {
-                lock (Pools[0])
-                {
-                    ZeroLocked = true;
-                    writer = Pools[0].Take();
-                    int Count = Pools[0].Count;
-                    if (Count < LowestScore)
-                    {
-                        LowestScore = Count;
-                        LowestIndex = 0;
-                    }
-
-                    writer.Reset();
-                    ZeroLocked = false;
-                    return writer;
-                }
-
-            }
-
-            if (OneLocked == false)
-            {
-                lock (Pools[1])
-                {
-                    OneLocked = true;
-                    writer = Pools[1].Take();
-                    int Count = Pools[1].Count;
-                    if (Count < LowestScore)
-                    {
-                        LowestScore = Count;
-                        LowestIndex = 1;
-                    }
-
-                    writer.Reset();
-                    OneLocked = false;
-                    return writer;
-                }
-
-            }
-
-            if (TwoLocked == false)
-            {
-                lock (Pools[2])
-                {
-                    TwoLocked = true;
-                    writer = Pools[2].Take();
-                    int Count = Pools[2].Count;
-                    if (Count < LowestScore)
-                    {
-                        LowestScore = Count;
-                        LowestIndex = 2;
-                    }
-
-                    writer.Reset();
-                    TwoLocked = false;
-                    return writer;
-                }
-
-            }
-
-            lock (Pools[0])
-            {
-                ZeroLocked = true;
-                writer = Pools[0].Take();
+                PooledNetworkWriter writer = Pool.Take();
                 writer.Reset();
-                ZeroLocked = false;
                 return writer;
             }
+
+
         }
-
-
 
         /// <summary>Return a writer to the pool.</summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Recycle(PooledNetworkWriter writer)
         {
-            /// UNITYSTATION CODE /// crazy logic getting the right one
-            if (writer == null) return;
-            LowestScore = LowestScore + 1;
-            Pool<PooledNetworkWriter> inLowest = Lowest[LowestIndex];
-            lock (inLowest)
+            lock (Pool)
             {
-                inLowest.Return(writer);
+                Pool.Return(writer);
             }
         }
     }
