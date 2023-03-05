106459da18680fd8308d41681fe79aedb93c8da0
Bod9001 <Bod9001@gmail.com>
2023-03-05 15:32:51 +0000

    Dumb fixe

--- Assets/Mirror/Editor/NetworkScenePostProcess.cs
+++ Assets/Mirror/Editor/NetworkScenePostProcess.cs
@@ -91,7 +91,11 @@ namespace Mirror
             // disable it
             // note: NetworkIdentity.OnDisable adds itself to the
             //       spawnableObjects dictionary (only if sceneId != 0)
-            identity.gameObject.SetActive(false);
+            if (identity.DontDisableOnPrepareSceneObject == false)
+            {
+                identity.gameObject.SetActive(false);
+            }
+
 
             // safety check for prefabs with more than one NetworkIdentity
 #if UNITY_2018_2_OR_NEWER
