ceae4d059263bbb0a7d693635ae26514a6cc6b80
Bod9001 <Bod9001@gmail.com>
2023-03-16 16:58:42 +0000

    Forgot to include the steps

--- Assets/Mirror/Core/NetworkIdentity.cs
+++ Assets/Mirror/Core/NetworkIdentity.cs
@@ -51,15 +51,15 @@ namespace Mirror
             {
                 if (_isDirty == false && value)
                 {
-
-                    if (observers != null)
+                    if (observers == null || observers.Count == 0)
                     {
-                        foreach (var Observer in observers)
-                        {
-                            Observer.Value.AddDirty(this);
-                        }
+                       return;
                     }
 
+                    foreach (var Observer in observers)
+                    {
+                        Observer.Value.AddDirty(this);
+                    }
                 }
                 _isDirty = value;
             }
@@ -1250,7 +1250,7 @@ namespace Mirror
                 comp.ClearAllDirtyBits();
             }
             /// UNITYSTATION CODE ///
-            isDirty = false;
+            _isDirty = false;
         }
 
         // this is used when a connection is destroyed, since the "observers" property is read-only
